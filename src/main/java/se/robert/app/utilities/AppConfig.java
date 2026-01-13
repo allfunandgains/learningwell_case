e7481977859a11f51ace97b70a60efdfc4482eb7
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-13 19:23:48 +0100

    Add new package 'support', including a configuration class AppConfig.java.

---
+++ src/main/java/se/robert/app/utilities/AppConfig.java
@@ -0,0 +1,18 @@
+package se.robert.app.utilities;
+
+import java.awt.Font;
+
+/**
+ * This class contains configuration data.
+ *
+ * @author Robert Kullman
+ */
+public final class AppConfig {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private AppConfig() {}
+
+
+}
