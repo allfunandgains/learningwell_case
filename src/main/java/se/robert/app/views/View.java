9e2a725e0e7aee0bb963783238f747020d089c8d
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-13 18:49:54 +0100

    Add initial UI setting for the View class.

--- src/main/java/se/robert/app/views/View.java
+++ src/main/java/se/robert/app/views/View.java
@@ -1,4 +1,32 @@
 package se.robert.app.views;
 
-public class View {
+import javax.swing.JFrame;
+
+/**
+ * Represents the main view of the application.
+ * Responsible for rendering the user interface and forwarding
+ * user interactions to the controller.
+ *
+ * @author Robert Kullman
+ */
+public class View extends JFrame {
+
+    /**
+     * Contructor for the View class.
+     */
+    public View() {
+        initUI();
+    }
+
+    /**
+     * Responsible for initializing the GUI.
+     */
+    private void initUI() {
+        this.setTitle("View");
+        this.setSize(800, 600);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+    }
 }
