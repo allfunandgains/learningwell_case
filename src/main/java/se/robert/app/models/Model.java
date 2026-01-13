faa0654eb63ef624592266818a0574c355a2f276
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-13 21:41:50 +0100

    Add helper method for retrieving dimension data from JSON-stat

--- src/main/java/se/robert/app/models/Model.java
+++ src/main/java/se/robert/app/models/Model.java
@@ -21,20 +21,43 @@ public class Model {
         this.apiClient = client;
 
         loadJsonData();
-        printUpdated();
 
     }
 
+    /**
+     * Attempts to fetch and parse the data into the root field.
+     */
     private void loadJsonData() {
         String jsonData = apiClient.getData(AppConfig.API_ADDRESS);
         root = JsonParser.parseString(jsonData).getAsJsonObject();
     }
 
-    // test
-    private void printUpdated() {
-        String testString = root
-                .get("updated").getAsString();
-        System.out.println(testString);
+    /**
+     * Returns the value corresponding to a certain index key.
+     * @param key The index key string.
+     * @return Integer value representing the specific data point.
+     */
+    private float getValue(String key) {
+        return root
+                .getAsJsonObject("value")
+                .get(key).getAsFloat();
+    }
+
+    /**
+     * Helper method for retrieving specific dimensions data
+     * from the JSON-stat response.
+     * @param root The JSON Object containing the data.
+     * @param dimension Name of the dimension to access.
+     * @param key The key for what specific dimension data to retrieve.
+     * @return Integer value for the specific data point.
+     */
+    private int getSpecificDimensionData(JsonObject root, String dimension, String key) {
+        return root.getAsJsonObject("dimension")
+                .getAsJsonObject(dimension)
+                .getAsJsonObject("category")
+                .getAsJsonObject("index")
+                .get(key)
+                .getAsInt();
     }
 
 }
