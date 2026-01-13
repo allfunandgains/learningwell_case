f542fdc4f3f15b990fa1937d21ac1fe8303d23d5
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-13 18:29:11 +0100

    Initialize Controller.java in main method, and make Controller initialize view and model.

--- src/main/java/se/robert/app/Main.java
+++ src/main/java/se/robert/app/Main.java
@@ -1,11 +1,15 @@
 package se.robert.app;
 
+import se.robert.app.controllers.Controller;
+
+import javax.swing.SwingUtilities;
+
 /**
  * The application entry point, responsible for initializing the Controller class.
  * @author Robert Kullman
  */
 public class Main {
     static void main() {
-        System.out.println("Hello World");
+        SwingUtilities.invokeLater(Controller::new);
     }
 }
--- src/main/java/se/robert/app/controllers/Controller.java
+++ src/main/java/se/robert/app/controllers/Controller.java
@@ -1,4 +1,15 @@
 package se.robert.app.controllers;
 
+import se.robert.app.models.Model;
+import se.robert.app.views.View;
+
 public class Controller {
+
+    private final View view;
+    private final Model  model;
+
+    public Controller() {
+        view = new View();
+        model = new Model();
+    }
 }
