5aab8d3cac396d3978b6cd06824962e0e0a9d1fb
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-13 18:17:35 +0100

    Add Main.java, which will serve as the entry point of the app.

---
+++ src/main/java/se/robert/app/Main.java
@@ -0,0 +1,11 @@
+package se.robert.app;
+
+/**
+ * The application entry point, responsible for initializing the Controller class.
+ * @author Robert Kullman
+ */
+public class Main {
+    static void main() {
+        System.out.println("Hello World");
+    }
+}
