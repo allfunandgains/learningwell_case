f4eab95b3ff8b2d6b594c674ac3a26bab3f641ed
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-13 19:45:29 +0100

    Add class documentation for Model.java.

--- src/main/java/se/robert/app/models/Model.java
+++ src/main/java/se/robert/app/models/Model.java
@@ -1,4 +1,15 @@
 package se.robert.app.models;
 
+/**
+ * The model component of the MVC pattern.
+ * Responsible for processing data retrieved from a web API.
+ *
+ * @author Robert Kullman
+ */
 public class Model {
+
+    public Model() {
+
+    }
+
 }
