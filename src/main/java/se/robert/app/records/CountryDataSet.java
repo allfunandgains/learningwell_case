67dad86914dde376f074a3f72513c0089b515929
Robert Kullman <4710870+allfunandgains@users.noreply.github.com>
2026-01-16 23:07:44 +0100

    Add missing documentation to records

--- src/main/java/se/robert/app/records/CountryDataSet.java
+++ src/main/java/se/robert/app/records/CountryDataSet.java
@@ -2,6 +2,13 @@ package se.robert.app.records;
 
 import java.util.LinkedList;
 
+/**
+ * A record containing a list of {@link YearData} objects and the
+ * corresponding country name.
+ * @param data list of {@link YearData} objects containing data
+ *             for each year in a time series.
+ * @param countryName the name of the country for which the data is relevant.
+ */
 public record CountryDataSet(
         LinkedList<YearData> data,
         String countryName
--- src/main/java/se/robert/app/records/YearData.java
+++ src/main/java/se/robert/app/records/YearData.java
@@ -2,9 +2,10 @@ package se.robert.app.records;
 
 /**
  * Immutable data transfer object representing values for a single year.
- * Holds values used to render a bar chart.
- *
- * @author Robert Kullman
+ *  Holds values used to render a bar chart.
+ * @param year the year integer value.
+ * @param maleValue male percentage value as a float.
+ * @param femaleValue female percentage value as a float.
  */
 public record YearData(
         int year,
